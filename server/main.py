53a12d63f6ad81e52cf91d07348573227ed55d36
ChadHomeLaptop <chadjcampbell@gmail.com>
2023-12-14 22:40:01 -0500

    start fastapi backend

---
+++ server/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# In-memory database (for testing purposes)
+comments = {}
+
+
+# Pydantic model for comment data
+class Comment(BaseModel):
+    name: str
+    comment: str
+
+
+# Read all comments for a given blog
+@app.get("/comments/{blog_name}", response_model=Comment)
+async def read_comments(blog_name: str):
+    if len(comments[blog_name]) == 0:
+        raise HTTPException(status_code=404, detail="Comments not found")
+    return comments[blog_name]
+
+
+# Create an comment
+@app.post("/comments/{blog_name}", response_model=Comment)
+async def create_item(blog_name: str, comment: Comment):
+    new_comment = comments[blog_name].append(comment)
+    return new_comment
